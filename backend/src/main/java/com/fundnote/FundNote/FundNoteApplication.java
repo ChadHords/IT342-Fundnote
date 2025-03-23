3384f4602231399547a09c154e59c3da8af8d55f
Dione Alfred Cabigas <dionealfred.cabigas@cit.edu>
2025-03-23 19:36:31 +0800

    Added Frontend and Backend base

---
+++ backend/src/main/java/com/fundnote/FundNote/FundNoteApplication.java
@@ -0,0 +1,13 @@
+package com.fundnote.FundNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundNoteApplication.class, args);
+	}
+
+}
