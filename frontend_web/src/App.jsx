81fed6579eb5c776d9a5bdb6de9e0a12f2d25721
Dione Alfred Cabigas <dionealfred.cabigas@cit.edu>
2025-03-25 16:17:09 +0800

    Updated frontend

--- frontend_web/src/App.jsx
+++ frontend_web/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
+import Signup from "./components/Signup";
+import Overview from "./components/Overview";
 
 const App = () => {
   return (
     <>
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/overview" element={<Overview />} />
+      </Routes>
+    </Router>
     </>
   );
 };
---
+++ frontend_web/src/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkDbNj6jbxYpfU5sknTpscdZj9xgXF79w",
+  authDomain: "it-342-fundnote.firebaseapp.com",
+  projectId: "it-342-fundnote",
+  appId: "1:327600413016:web:2177c55febf5d55c76ed2d",
+};
+
+// INITIALIZE FIREBASE
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
